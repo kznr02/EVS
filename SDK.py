0574473b2aa31b826880535d7452630f6112450f
kznr <clyde0203hy@outlook.com>
2023-07-17 23:21:17 +0800

    修复实现了目标边框的标注功能

--- SDK.py
+++ SDK.py
@@ -8,6 +8,9 @@ from PIL import Image
 from ctypes import *
 from GPCAlgo.CAlgoAAMeasurement import CAlgoAAMeasurement
 
+BOX_WIDTH = 40
+BOX_THICKNESS = 2
+
 class SRC_IMG(Structure):
     _fields_ = [("type", c_int), ("len", c_int), ("data", POINTER(c_uint16))]
 
@@ -89,14 +92,108 @@ class sdk():
         aa_image_path = os.path.join(self.res_path, '0.png')
         aa_image = cv2.imread(aa_image_path)
         aa_image_gray = cv2.cvtColor(aa_image, cv2.COLOR_BGR2GRAY)
-        _, binary = cv2.threshold(aa_image_gray, 180, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-        contours, hierachy = cv2.findContours(binary, cv2.RETR_LIST, cv2.CHAIN_APPROX_TC89_L1)
-        for cnt in contours:
-            rect = cv2.minAreaRect(cnt)
-            box = np.int_(cv2.boxPoints(rect))
 
-            cv2.drawContours(aa_image, [box], 0, (0, 255, 0), 2)
-        return binary
+        edge = cv2.Canny(aa_image_gray, 255, 0)
+        corners = cv2.goodFeaturesToTrack(edge, 1000, 0.01, 60)
+
+        plist = []
+        for i in range(0, 20):
+            x_o = int(corners[i][0][0])
+            y_o = int(corners[i][0][1])
+            plist.append((x_o, y_o))
+        plist.sort(key=lambda elem: elem[0])
+
+        plist_left = []
+        for i in range(0, 8):
+            plist_left.append(plist[i])
+        plist_left.sort(key=lambda elem: elem[1])
+        # print("left: ", plist_left)
+        left_points = [
+            ((plist_left[0][0] + plist_left[1][0]) // 2, (plist_left[0][1] + plist_left[1][1]) // 2),
+            ((plist_left[0][0] + plist_left[2][0]) // 2, (plist_left[0][1] + plist_left[2][1]) // 2),
+            ((plist_left[3][0] + plist_left[1][0]) // 2, (plist_left[3][1] + plist_left[1][1]) // 2),
+            ((plist_left[3][0] + plist_left[2][0]) // 2, (plist_left[3][1] + plist_left[2][1]) // 2),
+            ((plist_left[4][0] + plist_left[5][0]) // 2, (plist_left[4][1] + plist_left[5][1]) // 2),
+            ((plist_left[4][0] + plist_left[6][0]) // 2, (plist_left[4][1] + plist_left[6][1]) // 2),
+            ((plist_left[7][0] + plist_left[5][0]) // 2, (plist_left[7][1] + plist_left[5][1]) // 2),
+            ((plist_left[7][0] + plist_left[6][0]) // 2, (plist_left[7][1] + plist_left[6][1]) // 2)
+        ]
+        for i in left_points:
+            cv2.rectangle(aa_image, (i[0] - BOX_WIDTH, i[1] - BOX_WIDTH), (i[0] + BOX_WIDTH, i[1] + BOX_WIDTH), (0, 0, 255),
+                          BOX_THICKNESS)
+
+        plist_center = []
+        for i in range(8, 12):
+            plist_center.append(plist[i])
+        plist_center.sort(key=lambda elem: elem[1])
+        # print("center: ", plist_center)
+        center_points = [
+            ((plist_center[0][0] + plist_center[1][0]) // 2, (plist_center[0][1] + plist_center[1][1]) // 2),
+            ((plist_center[0][0] + plist_center[2][0]) // 2, (plist_center[0][1] + plist_center[2][1]) // 2),
+            ((plist_center[3][0] + plist_center[1][0]) // 2, (plist_center[3][1] + plist_center[1][1]) // 2),
+            ((plist_center[3][0] + plist_center[2][0]) // 2, (plist_center[2][1] + plist_center[2][1]) // 2)
+        ]
+        for i in center_points:
+            cv2.rectangle(aa_image, (i[0] - BOX_WIDTH, i[1] - BOX_WIDTH), (i[0] + BOX_WIDTH, i[1] + BOX_WIDTH), (0, 0, 255),
+                          BOX_THICKNESS)
+
+        plist_right = []
+        for i in range(12, 20):
+            plist_right.append(plist[i])
+        # print("right: ", plist_right)
+        plist_right.sort(key=lambda elem: elem[1])
+        right_points = [
+            ((plist_right[0][0] + plist_right[1][0]) // 2, (plist_right[0][1] + plist_right[1][1]) // 2),
+            ((plist_right[0][0] + plist_right[2][0]) // 2, (plist_right[0][1] + plist_right[2][1]) // 2),
+            ((plist_right[3][0] + plist_right[1][0]) // 2, (plist_right[3][1] + plist_right[1][1]) // 2),
+            ((plist_right[3][0] + plist_right[2][0]) // 2, (plist_right[3][1] + plist_right[2][1]) // 2),
+            ((plist_right[4][0] + plist_right[5][0]) // 2, (plist_right[4][1] + plist_right[5][1]) // 2),
+            ((plist_right[4][0] + plist_right[6][0]) // 2, (plist_right[4][1] + plist_right[6][1]) // 2),
+            ((plist_right[7][0] + plist_right[5][0]) // 2, (plist_right[7][1] + plist_right[5][1]) // 2),
+            ((plist_right[7][0] + plist_right[6][0]) // 2, (plist_right[7][1] + plist_right[6][1]) // 2)
+        ]
+        for i in right_points:
+            cv2.rectangle(aa_image, (i[0] - BOX_WIDTH, i[1] - BOX_WIDTH), (i[0] + BOX_WIDTH, i[1] + BOX_WIDTH), (0, 0, 255),
+                          BOX_THICKNESS)
+
+        UL_box_points = []
+        DL_box_points = []
+        CT_box_points = []
+        UR_box_points = []
+        DR_box_points = []
+        for i in range(0, 4):
+            UL_box_points.append(plist_left[i])
+            DL_box_points.append(plist_left[i + 4])
+            CT_box_points.append(plist_center[i])
+            UR_box_points.append(plist_right[i])
+            DR_box_points.append(plist_right[i + 4])
+        UL_center_point = \
+            ((((UL_box_points[0][0] + UL_box_points[3][0]) // 2) + (
+                        (UL_box_points[1][0] + UL_box_points[2][0]) // 2)) // 2,
+             (((UL_box_points[0][1] + UL_box_points[3][1]) // 2) + (
+                         (UL_box_points[1][1] + UL_box_points[2][1]) // 2)) // 2)
+        DL_center_point = \
+            ((((DL_box_points[0][0] + DL_box_points[3][0]) // 2) + (
+                        (DL_box_points[1][0] + DL_box_points[2][0]) // 2)) // 2,
+             (((DL_box_points[0][1] + DL_box_points[3][1]) // 2) + (
+                         (DL_box_points[1][1] + DL_box_points[2][1]) // 2)) // 2)
+        CT_center_point = \
+            ((((CT_box_points[0][0] + CT_box_points[3][0]) // 2) + (
+                        (CT_box_points[1][0] + CT_box_points[2][0]) // 2)) // 2,
+             (((CT_box_points[0][1] + CT_box_points[3][1]) // 2) + (
+                         (CT_box_points[1][1] + CT_box_points[2][1]) // 2)) // 2)
+        UR_center_point = \
+            ((((UR_box_points[0][0] + UR_box_points[3][0]) // 2) + (
+                        (UR_box_points[1][0] + UR_box_points[2][0]) // 2)) // 2,
+             (((UR_box_points[0][1] + UR_box_points[3][1]) // 2) + (
+                         (UR_box_points[1][1] + UR_box_points[2][1]) // 2)) // 2)
+        DR_center_point = \
+            ((((DR_box_points[0][0] + DR_box_points[3][0]) // 2) + (
+                        (DR_box_points[1][0] + DR_box_points[2][0]) // 2)) // 2,
+             (((DR_box_points[0][1] + DR_box_points[3][1]) // 2) + (
+                         (DR_box_points[1][1] + DR_box_points[2][1]) // 2)) // 2)
+
+        return aa_image
 
     def sdk_version(self) -> str:
         self.sdk.api_get_sdk_ver.restype = c_char_p
